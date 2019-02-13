82cb54ebf8104b0b19f8edb473ca14e6c7c227b8
C-M-Hanley <47262179+C-M-Hanley@users.noreply.github.com>
2019-02-12 19:29:58 -0500

    Update Program.cs

--- Week02/Program.cs
+++ Week02/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Week02
 {
@@ -7,7 +7,27 @@ namespace Week02
         static void Main(string[] args)
         {
             // Create variables and do things with them!
-            
+            Console.WriteLine("Hello world!");
+
+            string myName = "Coniah M. Hanley";
+             int myAge = 13;
+             string favColor = "Blue";
+             string myHob = "Nature";
+             bool isOld = false;
+             double myHeight = 4.9;
+             char favLetter = 'C';
+           
+           Console.WriteLine("My name is " + myName);
+            Console.WriteLine("My age is " + myAge);
+            Console.WriteLine("My favorite color is " + favColor);
+            Console.WriteLine("My hobbie is " + myHob);
+            Console.WriteLine("My height is " + myHeight + " feet tall");
+            Console.WriteLine("My first initial is " + favLetter);
+            Console.WriteLine("I am old enough to drive " + isOld);
+
+
+
+
         }
     }
 }
