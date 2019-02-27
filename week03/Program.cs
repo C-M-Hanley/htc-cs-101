6d94a5e9d624402492c95a73fcdce336ad7d806a
User <User@DEV-022777.OnBase.net>
2019-02-26 19:23:47 -0500

    did week 4 code along

--- week03/Program.cs
+++ week03/Program.cs
@@ -4,11 +4,12 @@ namespace week03
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
      
       Console.WriteLine("Guess a number between 1 and 10.");
       int num = Convert.ToInt32(Console.ReadLine());
+      
       
       if (num > 4){
           Console.WriteLine("Too High!");
---
+++ week04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Week04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+          {
+              bool Snowing = true;
+              bool HaveCarrot = true;
+              int temp = 40;
+              string clothes = "Outfit";
+              string name = "Kevin";
+
+              if (Snowing && temp <= 40 && (name != "Bob" || HaveCarrot)) {
+                  Console.WriteLine("I am a snow man!");
+              }
+
+              if(true || false) {
+                  Console.WriteLine("I!");
+              }
+
+              else{
+                  Console.WriteLine("o!");
+              }
+
+             
+
+          }
+ 
+string day = Console.ReadLine();
+          if (day == "mon" || day == "tue"){
+               Console.WriteLine("yay!");
+          }
+
+        }
+    }
+}
